c17c168c735b3b50b48683e4c273367ff2360aeb
pukaki <pukaki.ip@gmail.com>
2025-05-03 16:47:21 +0200

    static header

---
+++ src/header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>
+        Ja
+        <span id="elegant">m</span>
+        <span id="simple">m</span>
+        <span id="different">m</span>ing
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
